fc11597a14ef02cfe8d8b9833d93fd5d0a7ba8ca
Abymaly <ada.banaszczyk@gmail.com>
2018-06-09 00:22:23 +0200

    Binary Tree v2

--- src/org/abymaly/code/BinaryTree.java
+++ src/org/abymaly/code/BinaryTree.java
@@ -1,57 +1,158 @@
 package org.abymaly.code;
 
+import java.rmi.activation.ActivationGroup_Stub;
+import java.util.Arrays;
+
 public class BinaryTree<T> {
+    public int nodeCount;
 
     public class Node<T> {
         public Integer data;
-        public Node nextLeft;
-        public Node nextRight;
+        public Node<T> nextLeft;
+        public Node<T> nextRight;
+
 
         public Node(Integer data) {
             this.data = data;
-            }
+        }
 
         public Integer getData() {
             return data;
         }
+    }
 
-        public Node getNextLeft() {
-            return nextLeft;
-        }
+    Node<T> root;
 
-        public Node getNextRight() {
-            return nextRight;
-        }
+    public BinaryTree() {
+        this.root = null;
+        nodeCount = 0;
+    }
 
-        Node<T> root;
-        Node<T> actual;
+    public boolean isNextLeft(Node node) {
+        return node.nextLeft != null;
+    }
 
-        public void appendNode(Integer newData) {
-            Node ref = root;
-            Node<T> newNode = new Node<>(newData);
+    public boolean isNextRight(Node node) {
+        return node.nextRight != null;
+    }
 
-            if (root == null) {
-                root = newNode;
-                return;
-            }
-            actual = root;
+    public void appendNode(Integer newData) {
+        Node<T> newNode = new Node<>(newData);
 
-            while (actual != null) {
+        ++nodeCount;
 
-                if (newData.compareTo(root.getData()) < 0) {
-                    if (actual.nextLeft == null) {
-                        nextLeft = newNode;
-                    } else {
-                        nextLeft = actual;
-                    }
-                } else if (newData.compareTo(root.getData()) > 0)
-                    if (actual.nextRight == null) {
-                        nextRight = newNode;
-                    } else {
-                        nextRight = actual;
-                    }
-            }
+        if (root == null) {
+            root = newNode;
             return;
         }
+
+        Node<T> actual = root;
+
+        while (true) {
+
+            if (newData < actual.getData()) {
+                if (isNextLeft(actual)) {
+                    actual = actual.nextLeft;
+                } else {
+                    actual.nextLeft = newNode;
+                    return;
+                }
+            }
+
+            if (newData > actual.getData()) {
+                if (isNextRight(actual)) {
+                    actual = actual.nextRight;
+                } else {
+                    actual.nextRight = newNode;
+                    return;
+                }
+            }
+
+            if (newData == actual.getData()) {
+                return;
+            }
+        }
     }
+
+    public void searchValue(Integer findValue) {
+        int stepsCount = 1;
+
+        if (findValue == root.getData()) {
+            System.out.println(stepsCount);
+            return;
+        }
+
+        Node<T> actual = root;
+
+        while (findValue != actual.getData()) {
+
+            if (findValue < actual.getData()) {
+                if (isNextLeft(actual)) {
+                    ++stepsCount;
+                    actual = actual.nextLeft;
+                } else {
+                    System.out.println("There is no");
+                    return;
+                }
+            }
+
+            if (findValue > actual.getData()) {
+                if (isNextRight(actual)) {
+                    ++stepsCount;
+                    actual = actual.nextRight;
+                } else {
+                    System.out.println("There is no");
+                    return;
+                }
+            }
+        }
+        System.out.println(stepsCount);
+    }
+
+    public void delete(Integer deleteValue) {
+
+//        TODO: nie działa kasowanie przez dodawanie referencji do null. Wyszukuje dobrze, sprawdza dobrze, kasuje źle, do ogarniecia
+
+        Node<T> actual = root;
+
+        if (root.getData() == deleteValue) {
+            actual = root;
+        } else {
+
+        while (deleteValue != actual.getData()) {
+
+            if (deleteValue < actual.getData()) {
+                if (isNextLeft(actual)) {
+                    actual = actual.nextLeft;
+                } else {
+                    System.out.println("There is no");
+                    return;
+                }
+            }
+
+            if (deleteValue > actual.getData()) {
+                if (isNextRight(actual)) {
+                    actual = actual.nextRight;
+                } else {
+                    System.out.println("There is no");
+                    return;
+                }
+            }
+        }
+        }
+
+        if ((actual.nextLeft == null) && (actual.nextRight == null)) {
+            System.out.println("OK");
+            actual = null;
+            --nodeCount;
+        } else if (actual.nextRight == null) {
+            actual = actual.nextLeft;
+            --nodeCount;
+        } else {
+            actual = actual.nextRight;
+            --nodeCount;
+        }
+
+    }
+
 }
--- src/test/org/abymaly/code/BinaryTreeTest.java
+++ src/test/org/abymaly/code/BinaryTreeTest.java
@@ -2,6 +2,7 @@ package test.org.abymaly.code;
 
 import org.abymaly.code.BinaryTree;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -18,6 +19,17 @@ public class BinaryTreeTest {
     @Test
     public void testAppendNode01() {
         BinaryTree<Integer> bt = new BinaryTree<>();
+        bt.appendNode(2);
+        bt.appendNode(5);
+        bt.appendNode(8);
+        bt.appendNode(1);
+        System.out.println("size: " + bt.nodeCount);
+        bt.delete(1);
+//        bt.searchValue(2);
+//        bt.searchValue(5);
+//        bt.searchValue(8);
+        bt.searchValue(1);
+        System.out.println("size: " + bt.nodeCount);
 
     }
 } 
